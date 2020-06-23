f92194110e9002b3456daaa5f891ef9521f9b431
Mookyung Song <moono.song@ncsoft.com>
2020-06-23 12:54:33 +0900

    adding base resnet50

---
+++ base_networks/resnet50.py
@@ -0,0 +1,117 @@
+"""
+Just copy of keras_application/resnet_common.py
+
+Model: Resnet50
+channel_first,
+preact = False,
+use_bias = True
+"""
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+
+def block1(x, filters, kernel_size=3, stride=1, conv_shortcut=True, name=None):
+    data_format = 'channels_first'
+    bn_axis = 1
+
+    if conv_shortcut is True:
+        shortcut = layers.Conv2D(4 * filters, 1, strides=stride, name=name + '_0_conv', data_format=data_format)(x)
+        shortcut = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name=name + '_0_bn')(shortcut)
+    else:
+        shortcut = x
+
+    x = layers.Conv2D(filters, 1, strides=stride, name=name + '_1_conv', data_format=data_format)(x)
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name=name + '_1_bn')(x)
+    x = layers.Activation('relu', name=name + '_1_relu')(x)
+
+    x = layers.Conv2D(filters, kernel_size, padding='SAME', name=name + '_2_conv', data_format=data_format)(x)
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name=name + '_2_bn')(x)
+    x = layers.Activation('relu', name=name + '_2_relu')(x)
+
+    x = layers.Conv2D(4 * filters, 1, name=name + '_3_conv', data_format=data_format)(x)
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name=name + '_3_bn')(x)
+
+    x = layers.Add(name=name + '_add')([shortcut, x])
+    x = layers.Activation('relu', name=name + '_out')(x)
+    return x
+
+
+def stack1(x, filters, blocks, stride1=2, name=None):
+    x = block1(x, filters, stride=stride1, name=name + '_block1')
+    for i in range(2, blocks + 1):
+        x = block1(x, filters, conv_shortcut=False, name=name + '_block' + str(i))
+    return x
+
+
+def stack_fn(x):
+    x = stack1(x, 64, 3, stride1=1, name='conv2')
+    x = stack1(x, 128, 4, name='conv3')
+    x = stack1(x, 256, 6, name='conv4')
+    x = stack1(x, 512, 3, name='conv5')
+    return x
+
+
+def preprocess_inputs(image):
+    # image: rgb, 0.0 ~ 255.0
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    mean = mean[::-1]
+    std = std[::-1]
+    image_mean = tf.constant(mean, dtype=tf.float32) * 255.
+    image_std = tf.constant(std, dtype=tf.float32) * 255.
+
+    # convert to bgr
+    image = tf.reverse(image, axis=[-1])
+
+    # normalize
+    image = (image - image_mean) / image_std
+
+    # make channel first
+    image = tf.transpose(image, perm=[0, 3, 1, 2])
+    return image
+
+
+def get_model(res, pooling=None):
+    model_name = 'resnet50'
+    data_format = 'channels_first'
+    bn_axis = 1
+    img_input = layers.Input(shape=(res, res, 3), dtype='float32', name='input_1')
+
+    preprocessed = layers.Lambda(lambda _x: preprocess_inputs(_x))(img_input)
+
+    x = layers.ZeroPadding2D(padding=((3, 3), (3, 3)), name='conv1_pad', data_format=data_format)(preprocessed)
+    x = layers.Conv2D(64, 7, strides=2, use_bias=True, name='conv1_conv', data_format=data_format)(x)
+
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name='conv1_bn')(x)
+    x = layers.Activation('relu', name='conv1_relu')(x)
+
+    x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)), name='pool1_pad', data_format=data_format)(x)
+    x = layers.MaxPooling2D(3, strides=2, name='pool1_pool', data_format=data_format)(x)
+
+    x = stack_fn(x)
+
+    if pooling == 'avg':
+        x = layers.GlobalAveragePooling2D(name='avg_pool', data_format=data_format)(x)
+    elif pooling == 'max':
+        x = layers.GlobalMaxPooling2D(name='max_pool', data_format=data_format)(x)
+
+    inputs = img_input
+
+    # Create model.
+    model = models.Model(inputs, x, name=model_name)
+    return model
+
+
+def main():
+    from tensorflow.keras.applications.resnet50 import ResNet50
+
+    official = ResNet50(include_top=False, weights=None, input_shape=(224, 224, 3), pooling=None)
+    official.summary()
+
+    resnet50 = get_model(res=224)
+    resnet50.summary()
+    return
+
+
+if __name__ == '__main__':
+    main()
