52c15cdafcc8465b0099945c02f5f3432b317fe4
Mookyung Song <moono.song@ncsoft.com>
2020-06-25 17:17:46 +0900

    adding gradient tape will work?

--- test.py
+++ test.py
@@ -17,6 +17,7 @@ class Linear(tf.keras.models.Model):
 class MoCoTrainer(object):
     def __init__(self, batch_size, global_batch_size):
         self.use_tf_function = True
+        self.m = 0.999
         self.batch_size = batch_size
         self.global_batch_size = global_batch_size
 
@@ -24,42 +25,24 @@ class MoCoTrainer(object):
         self.encoder_k = Linear()
         return
 
-    # def _batch_shuffle(self, shuffle_idx):
-    #     replica_id = tf.distribute.get_replica_context().replica_id_in_sync_group
-    #
-    #     this_start = replica_id * self.batch_size
-    #     this_end = this_start + self.batch_size
-    #     this_idx = shuffle_idx[this_start:this_end]
-    #
-    #     tf.print(f'Replica {replica_id}: {this_idx}')
-    #     # tf.print(f'Replica {replica_id}: {d}')
-    #     return this_idx
-
-    def _batch_shuffle(self, all_gathered, strategy):
-        # convert to tf.Tensor
-        data = tf.concat(strategy.experimental_local_results(all_gathered), axis=0)
-
-        # create shuffled index for global batch size
-        all_idx = tf.range(self.global_batch_size)
-        shuffled_idx = tf.random.shuffle(all_idx)
-
-        # shuffle
-        shuffled_data = tf.gather(data, indices=shuffled_idx)
-        return shuffled_data, shuffled_idx
+    @tf.function
+    def _momentum_update_key_encoder(self):
+        for qw, kw in zip(self.encoder_q.weights, self.encoder_k.weights):
+            assert qw.shape == kw.shape
+            kw.assign(kw * self.m + qw * (1.0 - self.m))
+        return
 
     def forward_encoder_q(self, im_q):
         # compute query features
-        im_q = tf.expand_dims(im_q, axis=1)
-        q = self.encoder_q(im_q)    # queries: NxC
+        q = self.encoder_q(im_q)  # queries: NxC
         q = tf.math.l2_normalize(q, axis=1)
         return q
 
     def forward_encoder_k(self, all_images):
         # inputs are already shuffled,
         # just take shuffled data respect to their index will suffice
-        replica_id = tf.distribute.get_replica_context().replica_id_in_sync_group
-
         all_idx = tf.range(self.global_batch_size)
+        replica_id = tf.distribute.get_replica_context().replica_id_in_sync_group
         this_start = replica_id * self.batch_size
         this_end = this_start + self.batch_size
         this_idx = all_idx[this_start:this_end]
@@ -74,39 +57,48 @@ class MoCoTrainer(object):
         tf.print(f'Run {replica_id}: {im_k}')
         return k
 
-    def train(self, dist_dataset, strategy):
-        def dist_run_query_encoder(data):
-            per_replica_result = strategy.experimental_run_v2(fn=self.forward_encoder_q, args=(data,))
-            return per_replica_result
+    def train_step(self, inputs):
+        im_q, im_k, shuffled_data = inputs
 
-        def dist_run_key_encoder(data):
-            per_replica_result = strategy.experimental_run_v2(fn=self.forward_encoder_k, args=(data, ))
+        im_q = tf.expand_dims(im_q, axis=1)
+
+        with tf.GradientTape() as tape:
+            # compute query features
+            q = self.forward_encoder_q(im_q)  # queries: NxC
+
+            # should momentum update here?
+            self._momentum_update_key_encoder()
+
+            # compute key features with shuffled data
+            k = self.forward_encoder_k(shuffled_data)
+        return
+
+    def _batch_shuffle(self, all_gathered, strategy):
+        # convert to tf.Tensor
+        data = tf.concat(strategy.experimental_local_results(all_gathered), axis=0)
+
+        # create shuffled index for global batch size
+        all_idx = tf.range(self.global_batch_size)
+        shuffled_idx = tf.random.shuffle(all_idx)
+
+        # shuffle
+        shuffled_data = tf.gather(data, indices=shuffled_idx)
+        return shuffled_data, shuffled_idx
+
+    def train(self, dist_dataset, strategy):
+        def dist_run_train_step(inputs):
+            per_replica_result = strategy.experimental_run_v2(fn=self.train_step, args=(inputs,))
             return per_replica_result
 
         if self.use_tf_function:
-            dist_run_query_encoder = tf.function(dist_run_query_encoder)
-            dist_run_key_encoder = tf.function(dist_run_key_encoder)
+            dist_run_train_step = tf.function(dist_run_train_step)
 
-        for d in dist_dataset:
-            qs = dist_run_query_encoder(d)
+        for im_q, im_k in dist_dataset:
+            # shuffle data (maybe shuffle index is not needed)
+            shuffled_im_k, shuffled_idx_im_k = self._batch_shuffle(im_k, strategy)
 
-            # shuffle batch to fool batch normalization
-            shuffled_data, shuffled_idx = self._batch_shuffle(d, strategy)
-            tf.print(shuffled_data)     # [103 105 101 ... 107 102 100]
-            tf.print(shuffled_idx)      # [3 5 1 ... 7 2 0]
+            out = dist_run_train_step((im_q, im_k, shuffled_im_k))
 
-            ks = dist_run_key_encoder(shuffled_data)
-            # Run 0: [103 105 101 104]
-            # Run 1: [106 107 102 100]
-
-            tf.print(strategy.experimental_local_results(ks))   # ([103 105 101 104], [106 107 102 100])
-
-            # all_shuffled_idx = strategy.experimental_local_results(dist_batch_shuffle())
-            # all_shuffled_idx = tf.concat(all_shuffled_idx, axis=0)
-            # tf.print(all_shuffled_idx)
-            #
-            # shuffled_data = tf.gather(d, indices=all_shuffled_idx)
-            # tf.print(strategy.experimental_local_results(shuffled_data))
         return
 
 
@@ -117,8 +109,9 @@ def main():
     batch_size = 4
     global_batch_size = batch_size * strategy.num_replicas_in_sync
 
-    data = tf.range(100, 100 + global_batch_size * 10)
-    dataset = tf.data.Dataset.from_tensor_slices(data).batch(global_batch_size)
+    data_q = tf.range(100, 100 + global_batch_size * 5)
+    data_k = tf.range(200, 200 + global_batch_size * 5)
+    dataset = tf.data.Dataset.from_tensor_slices((data_q, data_k)).batch(global_batch_size)
     dist_dataset = strategy.experimental_distribute_dataset(dataset)
 
     # create moco trainer
