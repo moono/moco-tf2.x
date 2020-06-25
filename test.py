9c214841ff7f32b7e1ec7380edeb7c777e83a083
Mookyung Song <moono.song@ncsoft.com>
2020-06-25 14:58:01 +0900

    trial 2

--- test.py
+++ test.py
@@ -3,25 +3,25 @@ import tensorflow as tf
 
 class MoCoTrainer(object):
     def __init__(self, num_replicas_in_sync):
-        self.global_batch_size = 8 * num_replicas_in_sync
+        self.batch_size = 8
+        self.global_batch_size = self.batch_size * num_replicas_in_sync
         return
 
-    def _batch_shuffle(self):
-        batch_size = 8
+    def _batch_shuffle(self, shuffle_idx):
         replica_id = tf.distribute.get_replica_context().replica_id_in_sync_group
 
-        all_idx = tf.range(self.global_batch_size)
-        shuffle_idx = tf.random.shuffle(all_idx)
-
-        this_start = replica_id * batch_size
-        this_end = this_start + batch_size
+        this_start = replica_id * self.batch_size
+        this_end = this_start + self.batch_size
         this_idx = shuffle_idx[this_start:this_end]
 
         tf.print(f'Replica {replica_id}: {this_idx}')
         return
 
     def train(self, strategy):
-        strategy.experimental_run_v2(self._batch_shuffle)
+        all_idx = tf.range(self.global_batch_size)
+        shuffle_idx = tf.random.shuffle(all_idx)
+
+        strategy.experimental_run_v2(fn=self._batch_shuffle, args=(shuffle_idx,))
         return
 
 
