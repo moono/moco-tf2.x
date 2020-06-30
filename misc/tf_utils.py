93dfea567a882056f5df84d0523c69ff27886aa3
Mookyung Song <moono.song@ncsoft.com>
2020-06-30 11:05:04 +0900

    add spliting gpu for testing multi-gpu environment

--- misc/tf_utils.py
+++ misc/tf_utils.py
@@ -9,8 +9,34 @@ def allow_memory_growth():
             for gpu in gpus:
                 tf.config.experimental.set_memory_growth(gpu, True)
             logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            print(f'{len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs')
         except RuntimeError as e:
             # Memory growth must be set before GPUs have been initialized
             print(e)
     return
+
+
+def split_gpu_for_testing(mem_in_gb=4):
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            tf.config.experimental.set_virtual_device_configuration(
+                gpus[0],
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024 * mem_in_gb),
+                 tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024 * mem_in_gb)]
+            )
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(f'{len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs')
+        except RuntimeError as e:
+            print(e)
+    return
+
+
+def main():
+    # allow_memory_growth()
+    split_gpu_for_testing()
+    return
+
+
+if __name__ == '__main__':
+    main()
