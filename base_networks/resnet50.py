f11ff7d6f65163de5d7b73d6f959968bd46ceab8
Mookyung Song <moono.song@ncsoft.com>
2020-07-22 09:19:13 +0900

    rename layers

--- base_networks/resnet50.py
+++ base_networks/resnet50.py
@@ -1,11 +1,3 @@
-"""
-Just copy of keras_application/resnet_common.py
-
-Model: Resnet50
-channel_first,
-preact = False,
-use_bias = True
-"""
 import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.regularizers import l2
@@ -28,66 +20,64 @@ FC_KERNEL_INIT = tf.keras.initializers.VarianceScaling(scale=1/3, mode='fan_in',
 
 
 class Block1(models.Model):
-    def __init__(self, filters, kernel_size, stride, conv_shortcut, w_decay, name, **kwargs):
+    def __init__(self, filters, kernel_size, stride, downsample, w_decay, name, **kwargs):
         super(Block1, self).__init__(name=name, **kwargs)
         reg = l2(w_decay)
-        self.conv_shortcut = conv_shortcut
+        self.downsample = downsample
+        self.relu = layers.Activation('relu', name='relu')
 
-        if self.conv_shortcut:
-            self.conv_0 = layers.Conv2D(4 * filters, 1, strides=stride, name=name + '_0_conv', data_format=DATA_FORMAT,
-                                        use_bias=False, kernel_initializer=CONV_KERNEL_INIT, kernel_regularizer=reg)
-            self.bn_0 = layers.BatchNormalization(axis=BN_AXIS, momentum=BN_MOMENTUM, epsilon=BN_EPS, name=name + '_0_bn')
+        if self.downsample:
+            self.conv = layers.Conv2D(4 * filters, 1, strides=stride, name='downsample/0', data_format=DATA_FORMAT,
+                                      use_bias=False, kernel_initializer=CONV_KERNEL_INIT, kernel_regularizer=reg)
+            self.bn = layers.BatchNormalization(axis=BN_AXIS, momentum=BN_MOMENTUM, epsilon=BN_EPS, name='downsample/1')
 
-        self.conv_1 = layers.Conv2D(filters, 1, strides=stride, name=name + '_1_conv', data_format=DATA_FORMAT,
+        # 1x1
+        self.conv_1 = layers.Conv2D(filters, 1, strides=stride, name='conv1', data_format=DATA_FORMAT,
                                     use_bias=False, kernel_initializer=CONV_KERNEL_INIT, kernel_regularizer=reg)
-        self.bn_1 = layers.BatchNormalization(axis=BN_AXIS, momentum=BN_MOMENTUM, epsilon=BN_EPS, name=name + '_1_bn')
-        self.relu_1 = layers.Activation('relu', name=name + '_1_relu')
+        self.bn_1 = layers.BatchNormalization(axis=BN_AXIS, momentum=BN_MOMENTUM, epsilon=BN_EPS, name='bn1')
 
-        self.conv_2 = layers.Conv2D(filters, kernel_size, padding='SAME', name=name + '_2_conv', data_format=DATA_FORMAT,
+        # 3x3
+        self.conv_2 = layers.Conv2D(filters, kernel_size, padding='SAME', name='conv2', data_format=DATA_FORMAT,
                                     use_bias=False, kernel_initializer=CONV_KERNEL_INIT, kernel_regularizer=reg)
-        self.bn_2 = layers.BatchNormalization(axis=BN_AXIS, momentum=BN_MOMENTUM, epsilon=BN_EPS, name=name + '_2_bn')
-        self.relu_2 = layers.Activation('relu', name=name + '_2_relu')
+        self.bn_2 = layers.BatchNormalization(axis=BN_AXIS, momentum=BN_MOMENTUM, epsilon=BN_EPS, name='bn2')
 
-        self.conv_3 = layers.Conv2D(4 * filters, 1, name=name + '_3_conv', data_format=DATA_FORMAT,
+        # 1x1
+        self.conv_3 = layers.Conv2D(4 * filters, 1, name='conv3', data_format=DATA_FORMAT,
                                     use_bias=False, kernel_initializer=CONV_KERNEL_INIT, kernel_regularizer=reg)
-        self.bn_3 = layers.BatchNormalization(axis=BN_AXIS, momentum=BN_MOMENTUM, epsilon=BN_EPS, name=name + '_3_bn')
-
-        self.relu_out = layers.Activation('relu', name=name + '_out')
+        self.bn_3 = layers.BatchNormalization(axis=BN_AXIS, momentum=BN_MOMENTUM, epsilon=BN_EPS, name='bn3')
 
     def call(self, inputs, training=None, mask=None):
-        x = inputs
+        identity = tf.identity(inputs)
 
-        if self.conv_shortcut:
-            shortcut = self.conv_0(x)
-            shortcut = self.bn_0(shortcut, training=training)
-        else:
-            shortcut = tf.identity(x)
+        out = self.conv_1(inputs)
+        out = self.bn_1(out, training=training)
+        out = self.relu(out)
 
-        x = self.conv_1(x)
-        x = self.bn_1(x, training=training)
-        x = self.relu_1(x)
+        out = self.conv_2(out)
+        out = self.bn_2(out, training=training)
+        out = self.relu(out)
 
-        x = self.conv_2(x)
-        x = self.bn_2(x, training=training)
-        x = self.relu_2(x)
+        out = self.conv_3(out)
+        out = self.bn_3(out, training=training)
 
-        x = self.conv_3(x)
-        x = self.bn_3(x, training=training)
+        if self.downsample:
+            identity = self.conv(inputs)
+            identity = self.bn(identity, training=training)
 
-        x = shortcut + x
-        x = self.relu_out(x)
+        out += identity
+        out = self.relu(out)
 
-        return x
+        return out
 
 
 class Stack1(models.Model):
     def __init__(self, filters, n_blocks, stride, w_decay, name, **kwargs):
         super(Stack1, self).__init__(name=name, **kwargs)
 
-        self.block_1 = Block1(filters, 3, stride, conv_shortcut=True, w_decay=w_decay, name=f'{name}_block1')
+        self.block_1 = Block1(filters, 3, stride, downsample=True, w_decay=w_decay, name=f'0')
         self.blocks = list()
-        for i in range(2, n_blocks + 1):
-            self.blocks.append(Block1(filters, 3, 1, conv_shortcut=False,  w_decay=w_decay, name=f'{name}_block{i}'))
+        for i in range(1, n_blocks):
+            self.blocks.append(Block1(filters, 3, 1, downsample=False, w_decay=w_decay, name=f'{i}'))
 
     def call(self, inputs, training=None, mask=None):
         x = inputs
@@ -112,27 +102,27 @@ class Resnet50(models.Model):
         # layers
         self.preprocess = layers.Lambda(lambda x: self._preprocess_inputs(x))
 
-        self.conv1_pad = layers.ZeroPadding2D(padding=((3, 3), (3, 3)), name='conv1_pad', data_format=DATA_FORMAT)
-        self.conv1_conv = layers.Conv2D(64, 7, strides=2, name='conv1_conv', data_format=DATA_FORMAT,
-                                        use_bias=False, kernel_initializer=CONV_KERNEL_INIT, kernel_regularizer=reg)
-        self.conv1_bn = layers.BatchNormalization(axis=BN_AXIS, momentum=BN_MOMENTUM, epsilon=BN_EPS, name='conv1_bn')
-        self.conv1_relu = layers.Activation('relu', name='conv1_relu')
+        self.pad1 = layers.ZeroPadding2D(padding=((3, 3), (3, 3)), name='pad1', data_format=DATA_FORMAT)
+        self.conv1 = layers.Conv2D(64, 7, strides=2, name='conv1', data_format=DATA_FORMAT,
+                                   use_bias=False, kernel_initializer=CONV_KERNEL_INIT, kernel_regularizer=reg)
+        self.bn1 = layers.BatchNormalization(axis=BN_AXIS, momentum=BN_MOMENTUM, epsilon=BN_EPS, name='bn1')
+        self.relu1 = layers.Activation('relu', name='relu1')
         self.pool1_pad = layers.ZeroPadding2D(padding=((1, 1), (1, 1)), name='pool1_pad', data_format=DATA_FORMAT)
         self.pool1_pool = layers.MaxPooling2D(3, strides=2, name='pool1_pool', data_format=DATA_FORMAT)
 
-        self.stack_1 = Stack1(64, 3, stride=1, w_decay=w_decay, name='conv2')
-        self.stack_2 = Stack1(128, 4, stride=2, w_decay=w_decay, name='conv3')
-        self.stack_3 = Stack1(256, 6, stride=2, w_decay=w_decay, name='conv4')
-        self.stack_4 = Stack1(512, 3, stride=2, w_decay=w_decay, name='conv5')
+        self.stack_1 = Stack1(64, 3, stride=1, w_decay=w_decay, name='layer1')
+        self.stack_2 = Stack1(128, 4, stride=2, w_decay=w_decay, name='layer2')
+        self.stack_3 = Stack1(256, 6, stride=2, w_decay=w_decay, name='layer3')
+        self.stack_4 = Stack1(512, 3, stride=2, w_decay=w_decay, name='layer4')
 
         self.avg_pool = layers.GlobalAveragePooling2D(name='avg_pool', data_format=DATA_FORMAT)
 
         if self.with_projection_head:
-            self.mlp = layers.Dense(2048, activation='relu', name='mlp',
+            self.mlp = layers.Dense(2048, activation='relu', name='fc/0',
                                     kernel_initializer=FC_KERNEL_INIT, kernel_regularizer=reg, bias_regularizer=reg)
 
         # [N, classes]
-        self.last_dense = layers.Dense(self.classes, activation=None, name='probs',
+        self.last_dense = layers.Dense(self.classes, activation=None, name='fc/2',
                                        kernel_initializer=FC_KERNEL_INIT, kernel_regularizer=reg, bias_regularizer=reg)
         return
 
@@ -148,15 +138,17 @@ class Resnet50(models.Model):
         return
 
     def _preprocess_inputs(self, image):
-        # image: rgb, 0.0 ~ 1.0
-        image_mean = tf.constant([0.485, 0.456, 0.406], dtype=tf.float32)[None, None, None, :]
-        image_std = tf.constant([0.229, 0.224, 0.225], dtype=tf.float32)[None, None, None, :]
+        # image: rgb, (0.0 ~ 1.0)
+        # make channel first
+        image = tf.transpose(image, perm=[0, 3, 1, 2])
+
+        image_mean = [0.485, 0.456, 0.406]
+        image_std = [0.229, 0.224, 0.225]
+        image_mean = tf.constant(image_mean, dtype=tf.float32)[None, :, None, None]
+        image_std = tf.constant(image_std, dtype=tf.float32)[None, :, None, None]
 
         # normalize
         image = (image - image_mean) / image_std
-
-        # make channel first
-        image = tf.transpose(image, perm=[0, 3, 1, 2])
         return image
 
     def call(self, inputs, training=None, mask=None):
@@ -164,12 +156,12 @@ class Resnet50(models.Model):
 
         x = self.preprocess(x)  # [N, 3, 224, 224]
 
-        x = self.conv1_pad(x)   # [N, 3, 230, 230]
-        x = self.conv1_conv(x)  # [N, 64, 112, 112]
-        x = self.conv1_bn(x, training=training)     # [N, 64, 112, 112]
-        x = self.conv1_relu(x)  # [N, 64, 112, 112]
-        x = self.pool1_pad(x)   # [N, 64, 114, 114]
-        x = self.pool1_pool(x)  # [N, 64, 56, 56]
+        x = self.pad1(x)                        # [N, 3, 230, 230]
+        x = self.conv1(x)                       # [N, 64, 112, 112]
+        x = self.bn1(x, training=training)      # [N, 64, 112, 112]
+        x = self.relu1(x)                       # [N, 64, 112, 112]
+        x = self.pool1_pad(x)                   # [N, 64, 114, 114]
+        x = self.pool1_pool(x)                  # [N, 64, 56, 56]
 
         x = self.stack_1(x, training=training)  # [N, 256, 56, 56]
         x = self.stack_2(x, training=training)  # [N, 512, 28, 28]
@@ -196,6 +188,9 @@ def test_raw():
     resnet50 = Resnet50(resnet_params, name='encoder')
     resnet50.build((None, *resnet_params['input_shape']))
     resnet50.summary()
+
+    for w in resnet50.weights:
+        print(f'[{w.name}]: {w.shape}')
     return
 
 
